fae50945b52a498b015106d9d145705a2e7b526a
David Dubins <d.dubins@utoronto.ca>
2024-01-05 10:16:09 -0500

    Update wake_interrupt.ino

--- ATmega328Psleeps/wake_interrupt.ino
+++ ATmega328Psleeps/wake_interrupt.ino
@@ -1,34 +1,39 @@
+// Example: Wake on button push
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
+byte buttonPin=2;
 
-byte buttonPin=2;     //push button - goes LOW to wake up (PD2 will be interrupt, which is PCINT0) - PD2 is physical pin 4 on ATmega328 - FIX THIS ROUTINE
-
-void setup() {
+void setup(){
   Serial.begin(9600);
   pinMode(buttonPin,INPUT_PULLUP);
 }
  
-void loop() {
-  Serial.println("Going to sleep to save power.");
+void loop(){
+  Serial.println("Sleeping until button push.");
   sleep();  // run the sleep routine
-  Serial.println("Ready to do your bidding!!");
+  Serial.println("Ready to do your bidding!");
   delay(1000);
 }
 
-void sleep() {
-  // Two pin options for ATmega328: 0 (Pin 2) or 1 (Pin 3)
-  attachInterrupt(0, onWake, LOW);
-  delay(100); // warm-up time
+void sleep(){
+  // Two options for ATmega328: 0 (Pin 2) or 1 (Pin 3)
+  attachInterrupt(0, onWake, FALLING);
+  delay(100); // allow serial commands to finish
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  /* set_sleep_mode() options:
+     SLEEP_MODE_IDLE (least power savings)
+     SLEEP_MODE_ADC
+     SLEEP_MODE_PWR_SAVE
+     SLEEP_MODE_STANDBY
+     SLEEP_MODE_PWR_DOWN (most power savings) */
   sleep_enable();
-  sleep_mode(); // processor will sleep after this command.
-  sleep_disable(); // enable interrupts
-  detachInterrupt(0); // detach interrupt 0 during awake period 
+  sleep_mode(); // MCU will sleep here
+  sleep_disable(); // MCU will wake up here
+  detachInterrupt(0); // detach INT0 while awake 
 }
 
-void onWake()
-{
-  // Commands could go here, but if MCU just needs to wake up,
-  // leave empty.
+void onWake(){ // ISR
+  // Commands could go here. If MCU just needs to
+  // wake up, leave this empty.
 }
