b41e3d9bcfff3396e213774f045471cfbde1a255
David Dubins <d.dubins@utoronto.ca>
2024-12-10 13:26:34 -0500

    Create MCU1.ino

---
+++ 2UnosTalking/bareMin/MCU1.ino
@@ -0,0 +1,40 @@
+/* SoftwareSerial example, MCU1 to MCU2. This sketch is for MCU1 - the master, sending commands to MCU2.
+ * Author: D. Dubins
+ * Date: 10-Dec-24
+
+ * Description: This project illustrates how to get one MCU to send commands to another through a SoftwareSerial connection.
+ * This sketch is for the first MCU (MCU1). The second MCU (MCU2) will be powered parasitically through the Vin and GND pins of MCU1.
+ * This is hookup is optional though, they can be independently powered.
+ * 
+ * MCU1 reads an integer from the Serial Monitor, and sends a command to MCU2 through the SoftwareSerial connection.
+ * MCU2 receives the command, executes a function, then reports back to MCU1.
+
+ * Connections: (MCU1 - MCU2)
+ * Vin - Vin (or power independently)
+ * GND - GND
+ * Pin 2 - Pin 3 (RX - TX)
+ * Pin 3 - Pin 2 (TX - RX)
+*/
+
+#include <SoftwareSerial.h>       // include the software serial library
+SoftwareSerial MCU2Serial(2, 3);  // define software serial connection to MCU2 (RX:2, TX:3)
+
+void setup() {
+  Serial.begin(57600);      // start serial connection
+  MCU2Serial.begin(57600);  // start software serial connection with MCU2 (max speed for SoftwareSerial is 57600 for Uno)
+}
+
+void loop() {
+  int snd, rcv;  // define bytes for sending and receiving data
+  if (Serial.available()) {
+    snd = Serial.parseInt();  // store it to readKey
+    Serial.print("Sending to MCU2: ");  // messag
+    Serial.println(snd);    // print "snd" to Serial Monitor
+    MCU2Serial.print(snd);  // send "snd" to MCU2
+  }
+  if (MCU2Serial.available()){  // listen for response from MCU2
+    rcv = MCU2Serial.parseInt();  // store it to rcv
+    Serial.print("Receiving from MCU2: ");
+    Serial.println(rcv);  // send rcv to the regular Serial Monitor
+  }
+}
