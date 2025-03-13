22b467922b1040665d6f2cfe398da37d5ba0a270
David Dubins <d.dubins@utoronto.ca>
2025-03-13 00:59:46 -0400

    Update PID.ino

--- PID/PID.ino
+++ PID/PID.ino
@@ -1,8 +1,7 @@
-/*
-PID.ino: Control a drive element using a PID strategy
+/* PID.ino: Fan Control using a PID strategy
 Author: David Dubins
 Date: September 2018
-Last Updated: 12-Mar-25
+Last Updated: 13-Mar-25
 Connections:
 +5V to LM35 Pin 1
 Arduino Pin A0 to LM35 Pin 2
@@ -27,14 +26,13 @@ void setup() {
 
 void loop(){
   myPID(1.0, 0.0, 0.0); // call PID control here, entering
-                       // values for kP, kI, and kD (other
-                       // options available in PID subroutine)
-  delay(300);          // optional delay statement
+                        // values for kP, kI, and kD (other
+                        // options available in PID subroutine)
+  delay(500);           // delay statement
 }
 
 void myPID(float kP, float kI, float kD) {
-  /*
-  PID Algorithm:
+  /* PID Algorithm:
   MEASURED: Measured value for feedback 
   MESpin: pin to measure 
   SETPOINT: value you would like the system to converge to
@@ -52,8 +50,9 @@ void myPID(float kP, float kI, float kD) {
                        // is within 1 degC of the setpoint)
   float IntThresh=3.0; // narrow region of integral term
                        // (proximity to SET)
-  float ScaleFactor=-1.0; // Use this to change direction and
+  float ScaleFactor=-1.0; // Use this to change direction, and
                        // rescale DRIVE if necessary
+                       // ScaleFactor=-1: Cooling. ScaleFactor=1.0: Heating.
   float Error = 0.0;    
   float Integral = 0.0; // make this a global variable if not
                         // using the do-while loop
@@ -62,63 +61,50 @@ void myPID(float kP, float kI, float kD) {
   float D = 0.0;
   static float LAST=0.0; // remember LAST value after function exits
   long DRIVE = 0;      // to store DRIVE value
-  do {                 // You can get rid of the do..while
-                       // loop if you want the PID routine to
-                       // adjust the drive ONCE, i.e. not keep
-                       // going until you reach the SETPOINT.
-                       // This might be important if your
-                       // sketch needs to do other things!
-    MEASURED = analogRead(MESPin) * 500.0/1023.0; // convert
-                       // from divs to degC
-    Error = SETPOINT - MEASURED;
-    if (abs(Error)<TOLERANCE){
-      LAST = MEASURED;
-      I = 0;
-      Serial.println("SETPOINT within TOLERANCE.");
-      return; // leave function
-    }
-    if (abs(Error) < IntThresh){ // prevent integral wind-up
-                       // by only engaging it close to SET.
-      Integral = Integral + Error; // add to Error Integral
-    } else {     
-      Integral=0.0;    // zero Integral if out of bounds    
-    }        
-    P = Error*kP;      // calculate proportional term      
-    I = Integral*kI;   // integral term      
-    D = (LAST-MEASURED)*kD; // derivative term     
-    DRIVE = P + I + D; // Total DRIVE = P+I+D      
-    DRIVE = (long)(DRIVE*ScaleFactor); // used to reverse
-                    // direction, and scale DRIVE to be
-                    // in the range 0-255. 
-    DRIVE = constrain(DRIVE,0,255); // make sure DRIVE is an
-                       // integer between 0 and 255. An offset
-                       // can be used to get the DRIVE moving,
-                       // e.g. constrain(68+DRIVE,0,255).
-    Serial.print("SETPOINT: "); // info to serial monitor
-    Serial.print(SETPOINT);  
-    Serial.print(", MEASURED: ");
-    Serial.print(MEASURED);
-    Serial.print(", ERROR: ");
-    Serial.print(Error);
-    Serial.print(", P: ");  
-    Serial.print(P);  
-    Serial.print(", I: ");  
-    Serial.print(I);  
-    Serial.print(", D: ");  
-    Serial.print(D);  
-    Serial.print(", DRIVE: ");  
-    Serial.println(DRIVE);
-                       // replace all Serial.print commands
-                       // with the following command to
-                       // observe the response on the serial
-                       // plotter:
-    //Serial.println((String)SETPOINT+", "+(String)MEASURED);
-    LAST = MEASURED;   // save current value for next time
-    analogWrite(DRIVEPin, DRIVE); // send DRIVE as PWM signal
-    delay(500);        // optional delay (affects D term)
-  } while (abs(SETPOINT-MEASURED)>TOLERANCE);    // End of DO
-                       // loop condition. do..while will
-                       // always run at least once (while is
-                       // tested at the end of the loop).
-                       // Comment out if removing do loop. 
+  MEASURED = analogRead(MESPin) * 500.0/1023.0; //convert div to degC
+  Error = SETPOINT - MEASURED;
+  if (abs(Error)<TOLERANCE){
+    LAST = MEASURED;
+    I = 0;
+    Serial.println("SETPOINT within TOLERANCE.");
+    return; // leave function
+  }
+  if (abs(Error) < IntThresh){ // prevent integral wind-up
+                     // by only engaging it close to SET.
+    Integral = Integral + Error; // add to Error Integral
+  } else {     
+    Integral=0.0;    // zero Integral if out of bounds    
+  }        
+  P = Error*kP;      // calculate proportional term      
+  I = Integral*kI;   // integral term      
+  D = (LAST-MEASURED)*kD; // derivative term     
+  DRIVE = P + I + D; // Total DRIVE = P+I+D      
+  DRIVE = (long)(DRIVE*ScaleFactor); // used to reverse
+                  // direction, and scale DRIVE to be
+                  // in the range 0-255. 
+  DRIVE = constrain(DRIVE,0,255); // make sure DRIVE is an
+                     // integer between 0 and 255. An offset
+                     // can be used to get the DRIVE moving,
+                     // e.g. constrain(68+DRIVE,0,255).
+  Serial.print("SETPOINT: "); // info to serial monitor
+  Serial.print(SETPOINT);  
+  Serial.print(", MEASURED: ");
+  Serial.print(MEASURED);
+  Serial.print(", ERROR: ");
+  Serial.print(Error);
+  Serial.print(", P: ");  
+  Serial.print(P);  
+  Serial.print(", I: ");  
+  Serial.print(I);  
+  Serial.print(", D: ");  
+  Serial.print(D);  
+  Serial.print(", DRIVE: ");  
+  Serial.println(DRIVE);
+                     // replace all Serial.print commands
+                     // with the following command to
+                     // observe the response on the serial
+                     // plotter:
+  //Serial.println((String)SETPOINT+", "+(String)MEASURED);
+  LAST = MEASURED;   // save current value for next time
+  analogWrite(DRIVEPin, DRIVE); // send DRIVE as PWM signal
 }
